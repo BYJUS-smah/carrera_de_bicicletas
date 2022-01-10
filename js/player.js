f0d038a520baf3a67d7c86f70258718aa24ea1be
procodingclass <88050205+procodingclass@users.noreply.github.com>
2022-01-10 15:33:53 +0530

    Update player.js

--- js/player.js
+++ js/player.js
@@ -23,9 +23,9 @@ class Player {
     if (this.index === 1) {
       this.positionX = width / 2 - 320;
     } else if (this.index === 2) {
-      this.positionX = width / 2 - 150;
+      this.positionX = width / 2 - 100;
     } else if (this.index === 3) {
-      this.positionX = width / 2 + 150;
+      this.positionX = width / 2 + 100;
     } else if (this.index === 4) {
       this.positionX = width / 2 + 320;
     }
